0915d2bb7f300868888c2c44c205b0c39780e314
José Daniel <91283818+daniellsilva3@users.noreply.github.com>
2023-10-10 01:08:52 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import Banner from './Componentes/Banner/Banner';
+import Menu from './Componentes/Menu/Menu';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Banner/>
+      <Menu/>
+    
+      
+      
+    </div>
+  );
+}
+
+export default App;
